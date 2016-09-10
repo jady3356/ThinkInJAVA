eab1929112f169316e3219e388d3ca05a2d89477
Jady <taiyi.pei@gmail.com>
2016-09-10 10:43:52 +0800

    finish evil number

--- Charpter4_EvilNumber/src/Charpter4_EvilNumber.java
+++ Charpter4_EvilNumber/src/Charpter4_EvilNumber.java
@@ -1,12 +1,42 @@
 //:Charpter4_EvilNumber.java
 import java.util.*;
 
+class GetEvilNumber {
+	int digits[] = {0, 0, 0, 0};
+	int leftAndRight[] = {0, 0, 0, 0 ,0, 0, 0, 0 , 0, 0, 0, 0 };
+	int indexGlobal = 0;
+	public void convert(int input) {
+		String s = Integer.toString(input);
+		/*set digits*/
+		for(int index = 0; index < 4; index++){
+			digits[index] =  s.charAt(index) - '0';
+		}
+		/*set 12 possible left and right number*/
+		for(int index2 = 0; index2 < 4; index2++){
+			for(int index3 = index2 + 1; index3 < 4; index3++){
+				leftAndRight[indexGlobal++] = digits[index2] * 10 + digits[index3];
+				leftAndRight[indexGlobal++] = digits[index3] * 10 + digits[index2];
+			}
+		}
+		/*get evil number from 12 possible left and right number*/
+		for(int index4 = 0; index4 < indexGlobal; index4++) {
+			//System.out.println(leftAndRight[index4]);
+			for(int index5 = index4 + 1; index5 < indexGlobal; index5++){
+				if(leftAndRight[index4] * leftAndRight[index5] == input) {
+					System.out.println(input + " = " + leftAndRight[index4]  + " * " + leftAndRight[index5]);
+				}
+			}
+		}
+	} 
+	
+}
+
 public class Charpter4_EvilNumber {
 	public static void main (String[] args) {
-	
-		
-		
-		
-		
+		for(int i = 1001; i <= 9999; i++ ) {
+			GetEvilNumber EivlNumGet = new GetEvilNumber();
+			EivlNumGet.convert(i);
+		}
 	}
-} 
+} ///:~
+
--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,33 +1,33 @@
 //:HelloWorld/src/HelloWorld.java
 import java.util.*;
 
-/** This is the first Java program for me.
- * Display a string and time.
- * @author tpei
- * @version 0.0
- */
-
 class Exercise_1{
 	int vari_1;
 	char vari_2 = 0x41;
 }
 
+/**My first Java program 
+ * display some string and system property
+ * @author tpei
+ * @version 0.0
+*/
 public class HelloWorld {
-	/** Entry point to class & application.
+	/** Entry point to class and application.
 	 * @param args array of string arguments
-	 * @throws exception No exception thrown
+	 * @throws java.lang.IllegalArgumentException no exception
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world! It's: ");
 		System.out.println(new Date());
 		System.out.println(System.getProperty("user.name"));
-		System.out.println(
-				System.getProperty("java.library.path"));
+		System.out.println(System.getProperty("java.library.path"));
 		Exercise_1 excercise_1 = new Exercise_1();
 		System.out.println(excercise_1.vari_1);
 		System.out.println(excercise_1.vari_2);
 	}
-}///:~
+}/* Output: 
+*
+*///:~
 
 
 class Box{

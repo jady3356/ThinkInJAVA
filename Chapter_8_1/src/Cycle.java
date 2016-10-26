3d3b41d21d9dc60b8f8d9128d662c586b4b5e5f4
Jady <taiyi.pei@gmail.com>
2016-10-26 11:06:07 +0800

    Add chapter 8.1

---
+++ Chapter_8_1/src/Cycle.java
@@ -0,0 +1,42 @@
+//:/Cycle.java
+
+class BasicCycle {
+	public void run() {
+		System.out.println("BasicCycle.run()");
+	}
+}
+
+class Unicycle extends BasicCycle {
+	public void run() {
+		System.out.println("UniCycle.run()");
+	}
+}
+
+class Bicycle extends BasicCycle {
+	public void run() {
+		System.out.println("Bicycle.run()");
+	}
+}
+
+class Tricycle extends BasicCycle {
+	public void run() {
+		System.out.println("Tricycle.run()");
+	}
+}
+
+public class Cycle {
+	public static void Ride(BasicCycle c) {
+		c.run();
+	}
+	public static void main(String[] args) {
+		Unicycle UC = new Unicycle();
+		//UC.run();
+		Bicycle BC = new Bicycle();
+		//BC.run();
+		Tricycle TC = new Tricycle();
+		//TC.run();
+		Ride(UC);
+		Ride(BC);
+		Ride(TC);
+	}
+}
